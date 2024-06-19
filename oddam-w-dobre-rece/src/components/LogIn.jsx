e322ec047aca8fc8420a842e0526e9ea13fbfa5e
Alvin7914 <krzysztof.kamieniarz791@wp.pl>
2024-06-20 00:07:12 +0200

    login layout done, added red bottom border for errors in Contact component and inactive links for navigation

--- oddam-w-dobre-rece/src/components/LogIn.jsx
+++ oddam-w-dobre-rece/src/components/LogIn.jsx
@@ -1,12 +1,43 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Navigation from "./Navigation.jsx";
+import lineSvg from "../assets/Decoration.svg"
+import {Link} from "react-router-dom";
 
 const LogIn = () => {
+
+    useEffect(() => {
+        const NavLoginBtn = document.querySelector('.nav-login-btn');
+        const NavWhatIsItAboutBtn = document.querySelector('.nav-what-is-it-about');
+        const NavAboutUs = document.querySelector('.nav-about-us');
+        const NavFoundations = document.querySelector('.nav-foundations');
+        const NavContact = document.querySelector('.nav-contact');
+
+        NavLoginBtn.classList.add('disabled-link');
+        NavWhatIsItAboutBtn.classList.add('disabled-link');
+        NavAboutUs.classList.add('disabled-link');
+        NavFoundations.classList.add('disabled-link');
+        NavContact.classList.add('disabled-link');
+    }, []);
 
     return (
         <>
             <Navigation />
-            <div>LogIn</div>
+            <section className='login'>
+                <h2>Zaloguj się</h2>
+                <img src={lineSvg} alt="decorative-line"/>
+                <form className='login-form'>
+                    <label htmlFor="input-login-email">Email</label>
+                    <input id='input-login-email' type="email"/>
+                    <p className='error-msg d-none'>Podany email jest nieprawidłowy!</p>
+                    <label htmlFor="input-login-password">Hasło</label>
+                    <input id='input-login-password' type="password"/>
+                    <p className='error-msg d-none'>Podane hasło jest za krótkie!</p>
+                </form>
+                <div>
+                    <Link to='/rejestracja' className='register-btn'>Załóż konto</Link>
+                    <Link to='/' className='login-btn'>Zaloguj się</Link>
+                </div>
+            </section>
         </>
     )
 }
--- oddam-w-dobre-rece/src/components/Navigation.jsx
+++ oddam-w-dobre-rece/src/components/Navigation.jsx
@@ -8,15 +8,15 @@ const Navigation = () => {
     return (
         <nav>
             <ul className='login-list'>
-                <li className="login-item"><Link to='/logowanie'>Zaloguj</Link></li>
-                <li className="login-item"><Link to='/rejestracja'>Załóż konto</Link></li>
+                <li className="login-item"><Link to='/logowanie' className='nav-login-btn'>Zaloguj</Link></li>
+                <li className="login-item"><Link to='/rejestracja' className='nav-register-btn'>Załóż konto</Link></li>
             </ul>
             <ul className="nav-list">
                 <li className="nav-item"><Link to='/'>Start</Link></li>
-                <li className="nav-item"><ScrollLink to='simple-steps' smooth={true} duration={500}>O co chodzi?</ScrollLink></li>
-                <li className="nav-item"><ScrollLink to='about-us' smooth={true} duration={500}>O nas</ScrollLink></li>
-                <li className="nav-item"><ScrollLink to='who-we-help' smooth={true} duration={500}>Fundacja i organizacje</ScrollLink></li>
-                <li className="nav-item"><ScrollLink to='contact' smooth={true} duration={500}>Kontakt</ScrollLink></li>
+                <li className="nav-item"><ScrollLink className='nav-what-is-it-about' to='simple-steps' smooth={true} duration={500}>O co chodzi?</ScrollLink></li>
+                <li className="nav-item"><ScrollLink className='nav-about-us' to='about-us' smooth={true} duration={500}>O nas</ScrollLink></li>
+                <li className="nav-item"><ScrollLink className='nav-foundations' to='who-we-help' smooth={true} duration={500}>Fundacja i organizacje</ScrollLink></li>
+                <li className="nav-item"><ScrollLink className='nav-contact' to='contact' smooth={true} duration={500}>Kontakt</ScrollLink></li>
             </ul>
         </nav>
     )
