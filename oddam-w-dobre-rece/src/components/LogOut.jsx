43a8a6adc40d8fae3430a743f67f8d19ff161f98
Alvin7914 <krzysztof.kamieniarz791@wp.pl>
2024-06-20 02:16:07 +0200

    log out layout done

--- oddam-w-dobre-rece/src/components/LogOut.jsx
+++ oddam-w-dobre-rece/src/components/LogOut.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Navigation from "./Navigation.jsx";
+import lineSvg from "../assets/Decoration.svg";
+import {Link} from "react-router-dom";
 
 const LogOut = () => {
+
+    useEffect(() => {
+        const NavWhatIsItAboutBtn = document.querySelector('.nav-what-is-it-about');
+        const NavAboutUs = document.querySelector('.nav-about-us');
+        const NavFoundations = document.querySelector('.nav-foundations');
+        const NavContact = document.querySelector('.nav-contact');
+
+        NavWhatIsItAboutBtn.classList.add('disabled-link');
+        NavAboutUs.classList.add('disabled-link');
+        NavFoundations.classList.add('disabled-link');
+        NavContact.classList.add('disabled-link');
+    }, []);
 
     return (
         <>
             <Navigation />
-            <div>LogOut</div>
+            <section className='logout'>
+                <h2>Wylogowanie nastąpiło<br />pomyślnie!</h2>
+                <img src={lineSvg} alt="decorative-line"/>
+                <Link to='/' className='home-btn'>Strona główna</Link>
+            </section>
         </>
     )
 }
